23cae0d5f52ac554a5e2ef1b9efe1155e679fe93
Ilya Deydysh <kp50040@gmail.com>
2023-08-04 15:04:33 +0300

    fix adding rows in csv

--- webhook_listner.py
+++ webhook_listner.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import csv
 from flask_ngrok import run_with_ngrok
+from datetime import datetime
 
 app = Flask (__name__)
 run_with_ngrok(app)
@@ -8,17 +9,15 @@ run_with_ngrok(app)
 @app.route('/', methods = ['POST'])
 def webhook():
     data = request.get_json()
-    print(data["stt_answer"])
     try:
-        with open('STT_statistics.csv', 'w', encoding=('utf-8-sig'), newline='') as f:
+        with open('STT_statistics.csv', 'a', encoding=('utf-8-sig'), newline='') as f:
             writer = csv.writer(f, delimiter=';')
-            writer.writerow([data["destlinkname"], data["stt_answer"]])
+            writer.writerow([datetime.now(), data["stt_answer"]])
     except PermissionError:
         input('Close the window and press Enter.\n')
-        with open('STT_statistics.csv', 'w', encoding=('utf-8-sig'), newline='') as f:
+        with open('STT_statistics.csv', 'a', encoding=('utf-8-sig'), newline='') as f:
             writer = csv.writer(f, delimiter=';')
-            writer.writerow([data["destlinkname"], data["stt_answer"]])
-    print('Created STT_statistics.csv')
+            writer.writerow([datetime.now(), data["stt_answer"]])
 
     return 'Webhook recieved successfully', 200
 
